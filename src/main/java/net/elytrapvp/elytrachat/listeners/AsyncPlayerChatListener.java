5ee6a4fa63b334f8678a4c1f016c59e33f1b8bb5
colby <colbykrawczyk10@gmail.com>
2022-03-12 02:28:18 -0500

    Made plugin functional

--- src/main/java/net/elytrapvp/elytrachat/listeners/AsyncPlayerChatListener.java
+++ src/main/java/net/elytrapvp/elytrachat/listeners/AsyncPlayerChatListener.java
@@ -1,7 +1,9 @@
 package net.elytrapvp.elytrachat.listeners;
 
 import net.elytrapvp.elytrachat.ElytraChat;
+import net.elytrapvp.elytrachat.utils.chat.ChatUtils;
 import org.bukkit.Bukkit;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -28,6 +30,48 @@ public class AsyncPlayerChatListener implements Listener {
 
         Player player = event.getPlayer();
 
+        // Cancels the event so that the message does not get sent to general chat.
+        event.setCancelled(true);
+
+        // Sets the default format to "default".
+        String format = "default";
+
+        // Grabs the list of available formats from config.yml
+        ConfigurationSection formats = plugin.getSettingsManager().getFormats().getConfigurationSection("formats");
+
+        // Loops through all the available formats.
+        for(String str : formats.getKeys(false)) {
+            // Checks if the player has permission for that format.
+            if(player.hasPermission("format." + str)) {
+                // If so, sets the format to that.
+                format = str;
+
+                // Breaks out of the loop to prevent it from reassigning a format.
+                break;
+            }
+        }
+
+        // Creates the message to be sent.
+        String newMessage = "";
+
+        // Gets the selected format from the list of formats.
+        ConfigurationSection formatText = plugin.getSettingsManager().getFormats().getConfigurationSection("formats." + format);
+
+        // Loop through the format to add the text together.
+        for(String str : formatText.getKeys(false)) {
+            // Add the result to the new message.
+            newMessage += formatText.getString(str);
+        }
+
+        // Replace the placeholders with their intended text.
+        newMessage = newMessage
+                .replace("%player_name%", player.getName())
+                .replace("%message%", event.getMessage());
+
+        for(Player viewer : Bukkit.getOnlinePlayers()) {
+            ChatUtils.chat(viewer, newMessage);
+        }
+
         // Logs the message to MySQL.
         String name = player.getName();
         String uuid = player.getUniqueId().toString();
@@ -49,5 +93,4 @@ public class AsyncPlayerChatListener implements Listener {
             }
         });
     }
-
 }
